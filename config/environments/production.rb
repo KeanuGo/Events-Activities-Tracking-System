63f23888091f32a15689c0129aabebda79901ece
KeanuGo <keanuguanezogo@yahoo.com>
2018-06-05 09:20:40 +0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  ActionMailer::Base.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => "587",
+    :domain               => "gmail.com",
+    :user_name            => "eats.team123@gmail.com",
+    :password             => "eatsteam123",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
 end
